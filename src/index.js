126357f56e0217f1ac2fe852bb349963f628f50e
Vladimirsokolyansky <meloman1989@gmail.com>
2022-01-04 17:01:55 +0200

    originbranch

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 console.log("Script Launched");
-let detailsImage = document.querySelector(".details-images");
-let anchors = document.querySelectorAll(".thumbnails-anchor");
-let detailsTitle = document.querySelector(".details-title");
+let detailsImage = document.querySelector('.details-image');
+let detailsTitle = document.querySelector('.details-title');
+let mainContentEl = document.querySelector('.main-content');
+
+let anchors = document.querySelectorAll('.thumbnails-anchor');
 for(let i = 0;i < anchors.length; i++)
 {
     anchors[i].addEventListener("click", function(event)
     {
 event.preventDefault();
+setDetails();
 console.log(anchors[i].getAttribute("href"));
 setDetails(anchors[i]);
     })
@@ -21,4 +24,14 @@ function setDetails(anchor)
     let thumbnailsTitleEl = document.querySelector(thumbnailsTitleSelector);
     detailsTitle.textContent = `${thumbnailsTitleEl.textContent}: ${anchor.getAttribute("data-details-title")}`;
 
+}
+
+function showDetails()
+{
+    mainContentEl.classList.remove("hidden");
+}
+
+function hideDetails()
+{
+    mainContentEl.classList.add("hidden");
 }
